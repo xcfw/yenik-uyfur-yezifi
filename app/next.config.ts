b49ddbd766930fced27a8d72854a8e7d6750fbf5
xcfw <dr@gorsvet.kz>
2025-07-09 16:09:06 +0100

    baseurl folder serving next config

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,23 +1,28 @@
-import type {NextConfig} from 'next';
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  /**
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  // output: "export",
+  /**
+   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/basepath
+   */
+  basePath: "/urtranslit",
+  /**
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/react-strict-mode
+   */
+  // reactStrictMode: false,
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  /**
+   * @see https://nextjs.org/docs/pages/api-reference/next-config-js/eslint
+   */
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
